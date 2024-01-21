bc88ad237711193ac37bdde21825bfb5002bcbaf
uimi <roffrooff@gmail.com>
2024-01-21 16:42:03 +0200

    INIT: Project;

---
+++ src/main/java/net/mionex/firstplugin/FirstPlugin.java
@@ -0,0 +1,11 @@
+package net.mionex.firstplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FirstPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
