96f9e1c6bf714b8c809bb30356990dc81fb895a4
ART-SALI <rekudaasnin@gmail.com>
2024-01-21 18:18:38 +0200

    ADDED: onPlayerMove DELETED: Hello world

--- src/main/java/net/mionex/firstplugin/FirstPlugin.java
+++ src/main/java/net/mionex/firstplugin/FirstPlugin.java
@@ -1,11 +1,27 @@
 package net.mionex.firstplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class FirstPlugin extends JavaPlugin {
+public final class FirstPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        System.out.println("Hello world!");
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e){
+
+        if (e.getTo().getX() > 100 && e.getTo().getZ() > 100) {
+            e.setCancelled(true);
+            e.getPlayer().sendMessage(ChatColor.DARK_BLUE + "YOU are frozen");
+        }
+
     }
 }
